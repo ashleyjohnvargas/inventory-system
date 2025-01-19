a0e8e9e241e37a72f4ad7f5b3ea0fa0b43882990
ashleyjohnvargas <vashleyjohn@gmail.com>
2025-01-19 19:52:08 +0800

    comma_auto_appears_for_price_input

--- InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
+++ InventorySystem/obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56df60d8656957369b3d9383ad5769bfea4bb36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b5ba45a5881355c666794cec777534a4eb2a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
